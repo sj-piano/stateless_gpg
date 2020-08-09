0a2c213037940978b53fef01970dd841f312e8cf
StJohn Piano <stjohnpiano@gmail.com>
2020-08-09 13:55:16 +0100

    Add top-most __init__.py

---
+++ __init__.py
@@ -0,0 +1,8 @@
+# The real importing is done in src/__init__.py.
+# This file allows an external program to do this:
+# from statelessgpgpython2 import makeSignature, verifySignature
+# Note: We can't put the import statements in here and then use
+# "from .pytestImports import *" in e.g. pytest test files, because then we would get
+# the "Attempted relative import in non-package" error.
+# pytest -sq is usually run from the top-most directory.
+from src import *
