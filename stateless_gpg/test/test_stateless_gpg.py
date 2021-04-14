d8beb6192ae396d3e7c68f2c6e8832203d3d4aaf
StJohn Piano <stjohnpiano@gmail.com>
2021-04-14 14:35:02 +0100

    tabs -> 2 spaces

--- stateless_gpg/test/test_stateless_gpg.py
+++ stateless_gpg/test/test_stateless_gpg.py
@@ -8,13 +8,13 @@ dataDirPath = 'src/data'
 
 
 def test_hello_world():
-	data = "hello world\n"
-	privateKeyFilePath = join(dataDirPath, 'test_key_1_private_key.txt')
-	privateKey = open(privateKeyFilePath).read()
-	signature = gpg.makeSignature(privateKey, data)
-	publicKeyFilePath = join(dataDirPath, 'test_key_1_public_key.txt')
-	publicKey = open(publicKeyFilePath).read()
-	result = gpg.verifySignature(publicKey, data, signature)
-	assert result == True
+  data = "hello world\n"
+  privateKeyFilePath = join(dataDirPath, 'test_key_1_private_key.txt')
+  privateKey = open(privateKeyFilePath).read()
+  signature = gpg.makeSignature(privateKey, data)
+  publicKeyFilePath = join(dataDirPath, 'test_key_1_public_key.txt')
+  publicKey = open(publicKeyFilePath).read()
+  result = gpg.verifySignature(publicKey, data, signature)
+  assert result == True
 
 
