8666232e37bd2aa8539f97f2fa714588148c444f
StJohn Piano <stjohnpiano@gmail.com>
2021-04-14 14:52:09 +0100

    Convert bytes to strings

--- stateless_gpg/test/test_stateless_gpg.py
+++ stateless_gpg/test/test_stateless_gpg.py
@@ -9,10 +9,12 @@ join = os.path.join
 def test_hello_world():
   data = "hello world\n"
   privateKeyFilePath = '../data/test_key_1_private_key.txt'
-  privateKey = pkgutil.get_data(__name__, privateKeyFilePath)
+  privateKeyBytes = pkgutil.get_data(__name__, privateKeyFilePath)
+  privateKey = privateKeyBytes.decode('ascii')
   signature = gpg.makeSignature(privateKey, data)
   publicKeyFilePath = '../data/test_key_1_public_key.txt'
-  publicKey = pkgutil.get_data(__name__, publicKeyFilePath)
+  publicKeyBytes = pkgutil.get_data(__name__, publicKeyFilePath)
+  publicKey = publicKeyBytes.decode('ascii')
   result = gpg.verifySignature(publicKey, data, signature)
   assert result == True
 
