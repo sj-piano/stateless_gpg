c78b19db4542e6f0f1161e6bdfc02fb84bfa5386
StJohn Piano <stjohnpiano@gmail.com>
2021-04-14 14:57:12 +0100

    camelCase -> snake_case

--- stateless_gpg/test/test_stateless_gpg.py
+++ stateless_gpg/test/test_stateless_gpg.py
@@ -22,11 +22,11 @@ def test_hello_world():
   private_key_file = '../data/test_key_1_private_key.txt'
   private_key_bytes = pkgutil.get_data(__name__, private_key_file)
   private_key = private_key_bytes.decode('ascii')
-  signature = gpg.makeSignature(private_key, data)
+  signature = gpg.make_signature(private_key, data)
   public_key_file = '../data/test_key_1_public_key.txt'
   public_key_bytes = pkgutil.get_data(__name__, public_key_file)
   public_key = public_key_bytes.decode('ascii')
-  result = gpg.verifySignature(public_key, data, signature)
+  result = gpg.verify_signature(public_key, data, signature)
   assert result == True
 
 
