0990ca2d946868f36859b409edc575f275fcbb6b
StJohn Piano <stjohnpiano@gmail.com>
2021-04-22 19:36:39 +0100

    New code/__init__.py

---
+++ stateless_gpg/code/__init__.py
@@ -0,0 +1,47 @@
+# Imports
+import logging
+
+
+
+
+# Relative imports
+from .. import util
+from . import stateless_gpg
+
+
+
+
+# Set up logger for this module. By default, it produces no output.
+logger = logging.getLogger(__name__)
+logger.addHandler(logging.NullHandler())
+logger.setLevel(logging.ERROR)
+log = logger.info
+deb = logger.debug
+
+
+
+
+def setup(
+    log_level = 'error',
+    debug = False,
+    log_timestamp = False,
+    log_filepath = None,
+    ):
+  # Configure logger for this module.
+  util.module_logger.configure_module_logger(
+    logger = logger,
+    logger_name = __name__,
+    log_level = log_level,
+    debug = debug,
+    log_timestamp = log_timestamp,
+    log_filepath = log_filepath,
+  )
+  log('Setup complete.')
+  deb('Logger is logging at debug level.')
+  # Configure modules further down in this package.
+  stateless_gpg.setup(
+    log_level = log_level,
+    debug = debug,
+    log_timestamp = log_timestamp,
+    log_filepath = log_filepath,
+  )
