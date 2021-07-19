d2919911834359814a6195f30208822a9f2a7ebe
StJohn Piano <stjohnpiano@gmail.com>
2021-07-19 08:57:43 +0100

    More tests.

--- stateless_gpg/test/test_stateless_gpg.py
+++ stateless_gpg/test/test_stateless_gpg.py
@@ -78,8 +78,54 @@ def test_verify():
 
 
 
+def test_verify_failure():
+  # Verify signature using a different key.
+  data_file = '../data/data1.txt'
+  data = pkgutil.get_data(__name__, data_file).decode('ascii')
+  print("data = " + data.strip())
+  signature_file = '../data/data1_signature_by_test_key_1.txt'
+  signature = pkgutil.get_data(__name__, signature_file).decode('ascii')
+  public_key_file = '../data/morgan_industries_public_key.txt'
+  public_key = pkgutil.get_data(__name__, public_key_file).decode('ascii')
+  result = gpg.verify_signature(public_key, data, signature)
+  assert result is False
+  print("Could not verify signature.")
 
 
+
+
+def test_encrypt_and_decrypt():
+  # Encrypt some data.
+  data_file = '../data/data1.txt'
+  data = pkgutil.get_data(__name__, data_file).decode('ascii')
+  print("data = " + data.strip())
+  public_key_file = '../data/test_key_1_public_key.txt'
+  public_key = pkgutil.get_data(__name__, public_key_file).decode('ascii')
+  ciphertext = gpg.encrypt_data(public_key, data)
+  # Decrypt the data.
+  private_key_file = '../data/test_key_1_private_key.txt'
+  private_key = pkgutil.get_data(__name__, private_key_file).decode('ascii')
+  plaintext = gpg.decrypt_data(private_key, ciphertext)
+  print("plaintext = " + str(plaintext).strip())
+  success = plaintext == data
+  assert success is True
+
+
+
+
+def test_decrypt_failure():
+  data_file = '../data/data1.txt'
+  data = pkgutil.get_data(__name__, data_file).decode('ascii')
+  print("data = " + data.strip())
+  # Decrypt the data using a different key.
+  ciphertext_file = '../data/data1_encrypted_to_test_key_1.txt'
+  ciphertext = pkgutil.get_data(__name__, ciphertext_file).decode('ascii')
+  private_key_file = '../data/morgan_industries_private_key.txt'
+  private_key = pkgutil.get_data(__name__, private_key_file).decode('ascii')
+  plaintext = gpg.decrypt_data(private_key, ciphertext)
+  print("plaintext = " + str(plaintext).strip())
+  success = plaintext == data
+  assert success is False
 
 
 
